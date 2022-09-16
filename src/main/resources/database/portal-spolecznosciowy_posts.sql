639669aa275e11237a5fb56696cc0dcd3ed98aa8
Szymon Kochanowski <szymon.piotr.kochanowski@gmail.com>
2022-09-16 10:24:21 +0200

    Refactired database sql files

---
+++ src/main/resources/database/portal-spolecznosciowy_posts.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: portal-spolecznosciowy
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posts`
+--
+use heroku_fdd832cc737f6b1;
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `body` varchar(255) DEFAULT NULL,
+  `created` datetime(6) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `number_of_dislikes` int NOT NULL,
+  `number_of_likes` int NOT NULL,
+  `updated` datetime DEFAULT NULL,
+  `number_of_comments` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5lidm6cqbc7u4xhqpxm898qme` (`user_id`),
+  CONSTRAINT `FK5lidm6cqbc7u4xhqpxm898qme` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (45,'Eat, sleep, code, repeat!','2022-08-30 19:46:12.302910',78,1,1,NULL,3),(46,'Swagger OpenAPI czy jest lepszy niż Swagger 2?','2022-09-02 10:45:48.998951',78,0,0,NULL,0),(47,'Co lepszego jest w JUnit 5 od JUnit 4?','2022-09-02 10:46:31.518562',1,0,0,NULL,0),(48,'Czy lepiej jest wykorzystać ehcache czy redisa do zarządzania cachem?','2022-09-02 10:50:52.779538',74,0,0,NULL,0),(49,'Szklana pogoda a szyby nibiekie od telewizorów...','2022-09-02 10:52:06.958977',77,2,0,NULL,0),(63,'Kamil Brzeźiński i jego kanal: Jak nauczyć się programowania. Rządzi!!!!','2022-09-05 13:51:21.305173',88,0,4,'2022-09-05 14:05:10',0);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-16 10:09:34
